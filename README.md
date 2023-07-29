62fe2caf7c0471936afc241e7a6d1741480317d3
ticaki <59184294+ticaki@users.noreply.github.com>
2023-07-29 10:23:50 +0200

    weiter funktionen

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # iobroker-skript-sammlung
 Meine kleine Skriptsammlung zu iobroker
+
+Das wird im laufen der Zeit immer mehr
+
+Fertig ist aktuell 
+
+GlobalCreateFunktionen
+Damit erstellt man sicher Folder, Device, Channel
--- Sensorüberwachung.js
+++ Sensorüberwachung.js
@@ -1,8 +1,25 @@
 //bitte das folgende Skript noch drüber kopieren oder gleich als Globalskript anlegen
 //globaleCreateFunktionen.js
 
+const setFunctionToAllStates = false
+
 const enumFunctions = 'online' // die Funktion die in Aufzählung für dieses Skript zur Verfügung gestellt wird, muß den überwachten States zu geordnet werden
 
+const watchingDevice = {
+    adapter: [
+         '*.info.connection',
+        'system.adapter.*.alive'
+    ],
+    states: [       
+        '*.UNREACH',
+        'sonoff.*.Uptime',
+        'shelly.*.uptime',
+        'nuki.*.timestamp'
+    ]
+
+}
+
+//
 // Pfad an dem alles erstellt werden soll
 const path = '0_userdata.0.Kontrollzentrum.Sensorueberwachung'
 
@@ -15,11 +32,25 @@ const user = 'Tim'
 var msg = {}
 
 async function init() {
-    if (!existsObject(path)) await createFolderAsync(path, 'Geräteüberwachung')
+    if (setFunctionToAllStates ) {
+        for (const idPart of watchingDevice.states) {
+            let s = Array.prototype.slice.apply($('state(id='+idPart+')'))
+            for (const id of s) {
+                addToEnum('enum.functions.'+enumFunctions,id)
+            }
+        }
+    }
+
+    if (!existsObject(path)) await createFolderAsync(path, 'Ausfallüberwachung')
+    if (!existsObject(pathToState)) await createFolderAsync(pathToState, 'Geräteüberwachung')
+    if (!existsObject(pathToAdapter)) await createFolderAsync(pathToAdapter, 'Adapterüberwachung')
     work()
     return Promise.resolve(true);
 }
 
+const pathToState = path + '.gerät'
+const pathToAdapter = path + '.adapter'
+//system.adapter.admin.0.alive
 // Nachrichten werden nur einmal am Tag versendet, rücksetzen um 7:30 
 schedule('30 7 * * *', function() { msg = {}})
 
@@ -28,16 +59,18 @@ schedule('*/15 * * * *', work)
 schedule('15 10 * * 7', function(){work(true)})
 
 async function work(long = false){
-    let devs = $('state(functions='+enumFunctions+')')
-
+    let devs = Array.prototype.slice.apply($('state(functions='+enumFunctions+')'))
+    for (const sel of watchingDevice.adapter ) devs = Array.prototype.slice.apply($('state(id='+sel+')')).concat(devs)
     const now = new Date().getTime()
     for (let a=0; a<devs.length; a++ ) {
-        try {
+        //try {
             let dp = devs[a]
             let lc = getState(dp).lc // ts oder lc
             let v = {}
             v = await readDP(dp)
             let cts = v.zeit
+            if (long && !v["ts_langzeit_prüfung"]) continue
+            if (!v.activ) continue
             if (long) cts = v. langzeit
             let alarm = false;
             switch (v.art) {//"ts, lc, true, false Worauf geprüft werden soll"},
@@ -55,7 +88,7 @@ async function work(long = false){
                 if (long && !alarm) alarm = !getState(v.dp).ts + cts < now
                 break;
             } 
-            if (alarm)log(dp)
+            if (alarm)log(dp + ' nicht erreichbar (#dse1234#)', 'warn')
             if (alarm) {
                 if(msg[dp] === undefined) msg[dp] = {} 
                 msg[dp].ts = formatDate(lc,options)
@@ -70,7 +103,7 @@ async function work(long = false){
             } else {
                 delete msg[dp]
             }
-        } catch(e) {log(e);log(2)}
+        //} catch(e) {log(e);log(2)}
     }
     let message = ''
     for (let dp in msg) {
@@ -103,33 +136,60 @@ async function work(long = false){
 
 async function deleteDP(dp) {
     let id = dp.split('.').join('-')
-    let tPath = path +'.'+ id
+    let tPath = pathToState +'.'+ id
     if (existsObject(tPath)) {
-        for (let p in stateDef) {
+        for (let p in stateDef["_default"]) {
+            deleteState(tPath +'.'+ p)
+        }
+        deleteState(tPath )
+    }
+    tPath = pathToAdapter +'.'+ id
+    if (existsObject(tPath)) {
+        for (let p in stateDef["_default"]) {
             deleteState(tPath +'.'+ p)
         }
         deleteState(tPath )
     }
 }
 async function readDP(dp) {
+    let firstTag, lastTag , devTyp = 'state'
+    for (const d in stateDef.firstTag ) {
+        if (dp.startsWith(d)) {
+            firstTag = d
+            if (stateDef.firstTag[d].stateTyp) devTyp = stateDef.firstTag[d].stateTyp 
+        }
+    }
+    for (const d in stateDef.lastTag ) {
+        if (dp.endsWith(d)) {
+            lastTag = d
+            if (stateDef.lastTag[d].stateTyp !== undefined) devTyp = stateDef.lastTag[d].stateTyp 
+        }
+    }
     let id = dp.split('.').join('-')
-    let tPath = path +'.'+ id
+    let tPath = ''
+    if (devTyp == 'adapter') tPath = pathToAdapter +'.'+ id
+    else tPath = pathToState +'.'+ id
     let result = {} 
     if (existsObject(tPath)) {
-        for (let p in stateDef) {
+        for (let p in stateDef["_default"]) {
             result[p] = getState(tPath +'.'+ p).val
             if (!getState(tPath +'.'+ p).ack) setState(tPath +'.'+ p, result[p], true)
         }
     } else {
-        let name = getObject(dp.split('.').slice(0,-1).join('.')).common.name
+        let name 
+        if (existsObject(dp.split('.').slice(0,-1).join('.'))) name = getObject(dp.split('.').slice(0,-1).join('.')).common.name
+        else name = getObject(dp).common.name
         if (name.de !== undefined) name = name.de 
         await createDeviceAsync(tPath, name)
-        for (let p in stateDef) {
-            let o = {"type": stateDef[p].type, name:p, desc:stateDef[p].desc}
-            let def = stateDef[p].def
+
+        for (let p in stateDef["_default"]) {
+            let o = {"type": stateDef["_default"][p].type, name:p, desc:stateDef["_default"][p].desc}
+            let def = stateDef["_default"][p].def
+            if (lastTag && stateDef["lastTag"][lastTag][p]) def = stateDef["lastTag"][lastTag][p].def
+            else if (firstTag && stateDef["firstTag"][firstTag][p]) def = stateDef["firstTag"][firstTag][p].def
             if (p == "id") def = id
             if (p == "dp") def = dp
-            if (stateDef[p].states !== undefined) o.states = stateDef[p].states
+            if (stateDef["_default"][p].states !== undefined) o.states = stateDef["_default"][p].states
             await createStateAsync(tPath+'.'+p, def, o)
             result[p] = def
         }
@@ -172,12 +232,54 @@ async function readDP(dp) {
 }
 
 const stateDef = {
-    "id": {"type": "string", "def":"", "desc": "diese Id"},
-    "dp": {"type": "string", "def":"", "desc": "ursprüngliche ID"},
-    "art": {"type": "number", "def":0, "desc": "ts, lc, true, false Worauf geprüft werden soll", states:{"0": "Zeitstempel", "1": "Letzte Änderung", "2": "true = offline", "3":"false = offline"}},
-    "activ": {"type": "boolean", "def":true, "desc": "An/Auschalten"},
-    "zeit": {"type": "string", "def":"30m", "desc": "d,h,m"},
-    "langzeit": {"type": "string", "def":"14d", "desc": "Wie zeit, jedoch wird der Zeitstempel(ts) geprüft, ob dieser aktualisiert wurde"},
+    "_default": {
+        "id": {"type": "string", "def":"", "desc": "diese Id"},
+        "dp": {"type": "string", "def":"", "desc": "ursprüngliche ID"},
+        "art": {"type": "number", "def":0, "desc": "ts, lc, true, false Worauf geprüft werden soll", states:{"0": "Zeitstempel", "1": "Letzte Änderung", "2": "true = offline", "3":"false = offline"}},
+        "activ": {"type": "boolean", "def":true, "desc": "An/Auschalten"},
+        "zeit": {"type": "string", "def":"30m", "desc": "d,h,m"},
+        "ts_langzeit_prüfung": {"type": "boolean", "def":true, "desc": "Langzeitprüfung aktiv"},
+        "langzeit": {"type": "string", "def":"14d", "desc": "Wie zeit, jedoch wird der Zeitstempel(ts) einmal die Wochhe geprüft, ob dieser aktualisiert wurde"},
+    }, 
+    "firstTag": { 
+        "system": {
+            "stateTyp": "adapter",
+            "art": {"def":3},
+            "zeit": {"def":"15m"},
+            "ts_langzeit_prüfung": {"def":false}
+        }
+    },
+    "lastTag": { // das wird zuerst geprüft und überschreibt einstellungen in firstTag
+        "UNREACH": {
+            "stateTyp": "state",
+            "art": {"def":2},
+            "zeit": {"def":"1h"}
+        },
+        "info.connection": {
+            "stateTyp": "adapter",
+            "art": {"def":3},
+            "zeit": {"def":"15m"},
+            "ts_langzeit_prüfung": {"def":false}
+        }
+    }
+}
+
+async function addToEnum(enumName, newStateId) {
+    let myEnum = await getObjectAsync(enumName);
+    if (myEnum) {
+        let pos = myEnum.common.members.indexOf(newStateId);
+        if (pos === -1) {
+            try {
+                myEnum.common.members.push(newStateId);
+                myEnum.from = "system.adapter." + "0";
+                myEnum.ts = new Date().getTime();
+                log('enum:' + newStateId)
+                await setObjectAsync(enumName, myEnum);
+                Promise.resolve(true);
+            } catch (e) {log(e,'error')}
+        }
+    }
+    Promise.resolve(false);
 }
 
 init()
